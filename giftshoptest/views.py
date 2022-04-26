2ba881a23d1243c3f0af0ab50b669d55ad3de611
j998123 <lj20101617@gmail.com>
2022-04-26 19:16:26 +1000

    a little update again again

--- giftshoptest/views.py
+++ giftshoptest/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
-
 from .models import *
 from cart.cart import Cart
 import re
