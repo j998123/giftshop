0fc40bcf80c4afc2252ca9cf666a2f708d8758d0
j998123 <lj20101617@gmail.com>
2022-05-01 23:20:43 +1000

    product detail update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -66,7 +67,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
